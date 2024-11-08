a096ecbaef14e61494123a996fd3dd9732583535
Moritz E. Beber <midnighter@posteo.net>
2024-11-08 01:18:35 +0100

    chore: adjust to optional dependencies

--- example/app/app.py
+++ example/app/app.py
@@ -23,7 +23,8 @@ from structlog.contextvars import bind_contextvars, clear_contextvars
 
 from knowledge_chat.application import KnowledgeChat, UserDTO
 from knowledge_chat.infrastructure.domain.service import LangchainDomainServiceRegistry
-from knowledge_chat.infrastructure.settings import Neo4jSettings, OllamaSettings
+from knowledge_chat.infrastructure.settings.neo4j_settings import Neo4jSettings
+from knowledge_chat.infrastructure.settings.ollama_settings import OllamaSettings
 
 
 logger = structlog.get_logger()
